c3d72edb3fa4f5240c670bd8e02ef40fb018d276
Tyler Gaw <me@tylergaw.com>
2013-04-19 08:49:18 -0400

    Getting set up with this mess

---
+++ README.md
@@ -0,0 +1,3 @@
+# Again with this?
+
+A site of little reminders to myself. Don't be dummy.
---
+++ conf/compass/config.rb
@@ -0,0 +1,9 @@
+# This is a compass (sass/stylesheet) configuration file.
+css_dir = "../../css"
+sass_dir = "../../css/scss"
+images_dir = "../../images"
+output_style = :compressed
+line_comments = true
+relative_assets = true
+preferred_syntax = :scss
+asset_cache_buster :none
